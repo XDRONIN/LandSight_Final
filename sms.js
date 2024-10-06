bdbf1ac1afbc2dfac66daf048a5f275ea94500ec
XDRONIN <akashprakashmattathil@gmail.com>
2024-10-06 21:24:18 +0530

    finalization

--- sms.js
+++ sms.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const twilio = require('twilio');
-const cors = require('cors');
+import express from "express";
+import twilio from "twilio";
+import cors from "cors";
 const app = express();
 
 //Twilio credentials
-const accountSid = 'AC7dc844f9f544f0fb90c3b81e0e1835d9'; //Account SID
-const authToken = 'b3ff473faae08b33ab30f2fbb3c69562'; //Auth Token
+const accountSid = "AC035656119bb814c070996cb79acdab20"; //Account SID
+const authToken = "d3ed0f55ff27492068e22a9c6bdcd557"; //Auth Token
 const client = twilio(accountSid, authToken);
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/send-sms', async (req, res) => {
-    const { to, message } = req.body;
-    
-    try {
-        await client.messages.create({
-            body: message,
-            to: to,
-            from: '+15204472656' // Twilio number
-        });
-        res.json({ success: true });
-    } catch (error) {
-        res.json({ success: false, error: error.message });
-    }
+app.post("/send-sms", async (req, res) => {
+  const { to, message } = req.body;
+
+  try {
+    await client.messages.create({
+      body: message,
+      to: to,
+      from: "+19259564244", // Twilio number
+    });
+    res.json({ success: true });
+  } catch (error) {
+    res.json({ success: false, error: error.message });
+  }
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, () => console.log("Server running on port 3000"));
